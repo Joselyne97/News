1c72d7c36793a217dd55214d370067e7ed2e06ea
Joselyne97 <joselynejojo740@gmail.com>
2019-09-15 19:56:07 +0200

    I added the search

--- app/main/views.py
+++ app/main/views.py
@@ -1,6 +1,6 @@
 from flask import render_template
 from . import main
-from ..requests import get_newss
+from ..requests import get_newss,get_articles,search_news
 # from .forms import ReviewForm
 # from ..models import Review
 from ..models import News
@@ -22,6 +22,10 @@ def index():
     science_news = get_newss('science')
 
     title = 'Home - Welcome to The News Review Website Online'
+    search_news = request.args.get('news_query')
+    if search_news:
+        return redirect(url_for('main.search',wanted_news = search_news))
+    else:
     return render_template('index.html', title = title, business_news = business_news, technology_news = technology_news,sports_news = sports_news, general_news = general_news, entertainment_news = entertainment_news, health_news = health_news, science_news = science_news)
 
 @main.route('/news/<id>')
@@ -29,10 +33,21 @@ def articles(id):
    '''
    view articles page
    '''
-   print('test')
+  
    articles = get_articles(id)
   
    title = 'Home - Welcome to The News Review Website Online those are articles'
    return render_template('articles.html',title= title,articles = articles)
 
+@main.route('/search/<wanted_news>')
+def search(wanted_news):
+    '''
+    View function to display the search results
+    '''
+
+    news_name_list = wanted_news.split(' ')
+    news_name_format = '+'.join(news_name_list)
+    searched_wanteds = search_news(news_name_format)
+    title = f'search results for {wanted_news}'
+    return render_template('search.html',news_wanteds = searched_wanteds, w =wanted_news,title=title)
 
