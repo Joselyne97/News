46c89d83ac3a1626d493c8d0a8368e2c62646269
Joselyne97 <joselynejojo740@gmail.com>
2019-09-16 10:40:52 +0200

    I added the image in articles

--- app/models.py
+++ app/models.py
@@ -25,32 +25,3 @@ class Articles:
        self.image = image
        
 
-# class Review:
-
-#     all_reviews = []
-
-#     def __init__(self,news_name,title,imageurl,review):
-#         self.news_name = news_name
-#         self.description = description
-#         self.category = category
-#         self.review = review
-
-
-#     def save_review(self):
-#         Review.all_reviews.append(self)
-
-
-#     @classmethod
-#     def clear_reviews(cls):
-#         Review.all_reviews.clear()
-
-#     @classmethod
-#     def get_reviews(cls,name):
-
-#         response = []
-
-#         for review in cls.all_reviews:
-#             if review.news_name == name:
-#                 response.append(review)
-
-#         return response
